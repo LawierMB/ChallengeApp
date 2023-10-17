e109c4e81dbe023d774970b7adae0033a8b590ff
Lawier <lawier@wp.pl>
2023-10-17 15:19:45 +0200

    Zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,33 +1,56 @@
-﻿// Zadanie 1
-//Console.WriteLine("Hello, Marek! \n");
-//Console.WriteLine("wciśnij dowolny klawisz!");
-//Console.ReadKey();
+﻿// Przygotój program, który policzy ile jakich cyfr występuje w podanej liczbie
 
-// Zadanie 2
+var number = 4566;
+var numberToString = number.ToString();
+var letters = numberToString.ToArray();
+int[] counterLeters = new int[10];
 
-var imie = "Ania";
-var plec = 'k';
-var wiek = 29;
 
-Console.WriteLine("przyjęto dane do sprawdzenia: ");
-Console.WriteLine("Imię: " + imie + " płeć: " + plec + " Wiek: " + wiek + " :");
-
-if (plec == 'k')
+/*for (int index = 0; index < 10; index++)
 {
-    if (imie == "Ewa" && wiek == 33)
+    counterLeters[index] = index;
+}*/
+
+
+
+foreach (var letter in letters)
+{
+    if (letter == '0')
     {
-        Console.WriteLine("Ewa, lat 33 ");
+        counterLeters[0]++;
+    }else if (letter == '1')
+    {
+        counterLeters[1]++;
+    }else if (letter == '2')
+    {
+        counterLeters[2]++;
+    }else if (letter == '3')
+    {
+        counterLeters[3]++;
+    }else if (letter == '4')
+    {
+        counterLeters[4]++;
+    }else if (letter == '5')
+    {
+        counterLeters[5]++;
+    }else if (letter == '6')
+    {
+        counterLeters[6]++;
+    }else if (letter == '7')
+    {
+        counterLeters[7]++;
+    }else if (letter == '8')
+    {
+        counterLeters[8]++;
+    }else
+    {
+        counterLeters[9]++;
     }
-    else if (wiek < 30)
-    {
-        Console.WriteLine("Kobieta poniżej 30 lat");
-    } 
+    
 }
-else if (wiek < 18)
+
+Console.WriteLine("Podliczenie cyfr w liczbie: " + number + " Wygląda następująco: ");
+for (int index = 0; index < 10; index++)
 {
-    Console.WriteLine("Niepelnoletni mężczyzna.");
-}
-else
-{
-    Console.WriteLine("Pełnoletni mężczyzna.");
+    Console.WriteLine("Cyfra: " + index + " Wystąpienie: " + counterLeters[index] + " Razy !");
 }
