ad5f74819f225ef5417e243487165726e87af0e7
Lawier <lawier@wp.pl>
2023-10-23 23:54:06 +0200

    Zadanie dzień 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,34 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        // tworzenie nowego kontenera listy "score" do przechowywania punktów
+        private List<int> score = new List<int>();
+
+        public Employee(string FirstName, string LastName, int Age) 
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Age = Age;
+        }
+        
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+
+        public int ResultPoint
+        {
+            get
+            {
+                // getter zwraca podsumowanie punktów listy "score"
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int scorePoint)
+        {
+            // metoda przy każdym wywołaniu dodaje kolejne punkty do listy
+            this.score.Add(scorePoint);
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,18 +1,50 @@
 ﻿using ChallengeApp;
 
-User user1 = new User("Marek", "Password1");
-User user2 = new User("Norbert", "Password2");
-User user3 = new User("Gosia", "Password3");
-User user4 = new User("Damian", "Password4");
+List<Employee> workers = new List<Employee>();
 
-Console.WriteLine("Uąytkownik 1: " + user1.Login + " " + user1.Password);
-Console.WriteLine("Uąytkownik 3: " + user3.Login + " " + user3.Password);
+Employee worker1 = new Employee("Antoni", "Szpak", 32);
+workers.Add(worker1);
+Employee worker2 = new Employee("Marianna", "Wieczorek", 25);
+workers.Add(worker2);
+Employee worker3 = new Employee("Artem", "Dziarski", 30);
+workers.Add(worker3);
 
-// przypisanie punktów użytkownikowi user1 przy pomocy metody AddScore()
-user1.AddScore(5);
-user1.AddScore(2);
-user1.AddScore(1);
+worker1.AddScore(3);
+worker1.AddScore(5);
+worker1.AddScore(8);
+worker1.AddScore(2);
+worker1.AddScore(4);
 
-// wyświetla podsumowanie punktów user1.ResuldPoint
-Console.WriteLine("Zebrane punkty przez użytkownika: " + user1.Login + " to : " + user1.ResultPoint);
+worker2.AddScore(2);
+worker2.AddScore(3);
+worker2.AddScore(8);
+worker2.AddScore(9);
+worker2.AddScore(1);
 
+worker3.AddScore(5);
+worker3.AddScore(1);
+worker3.AddScore(9);
+worker3.AddScore(2);
+worker3.AddScore(3);
+
+Console.WriteLine("Zebrane punkty przez pracownika: " 
+    + worker1.FirstName + " " + worker1.LastName +" to : " + worker1.ResultPoint);
+Console.WriteLine("Zebrane punkty przez pracownika: " 
+    + worker2.FirstName + " " + worker2.LastName +" to : " + worker2.ResultPoint);
+Console.WriteLine("Zebrane punkty przez pracownika: " 
+    + worker3.FirstName + " " + worker3.LastName +" to : " + worker3.ResultPoint);
+
+int maxResult = -1;
+Employee workerWithMaxResult = null;
+
+foreach (var worker in workers)
+{
+    if (worker.ResultPoint > maxResult)
+    {
+        workerWithMaxResult = worker;
+        maxResult = worker.ResultPoint;
+    }
+}
+Console.WriteLine("\nNajlepszy wynik osiągnął pracownik: " 
+    + workerWithMaxResult.FirstName + " " + workerWithMaxResult.LastName 
+    + " z " + workerWithMaxResult.ResultPoint + " punktami !");
