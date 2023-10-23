c65de8f269afc4d3acca5ef231388ab7ad7480ac
Lawier <lawier@wp.pl>
2023-10-23 14:33:30 +0200

    modyfikacja programu i przeniecienie klasy do oddzielnego pliku.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,4 +1,4 @@
-﻿// Pierwsza klasa
+﻿using ChallengeApp;
 
 User user1 = new User("Marek", "Password1");
 User user2 = new User("Norbert", "Password2");
@@ -16,35 +16,3 @@ user1.AddScore(1);
 // wyświetla podsumowanie punktów user1.ResuldPoint
 Console.WriteLine("Zebrane punkty przez użytkownika: " + user1.Login + " to : " + user1.ResultPoint);
 
-class User
-{
-    // tworzenie nowego kontenera listy "score" do przechowywania punktów
-    private List<int> score = new List<int>(); 
-
-    public User(string login, string password)
-    {
-        this.Login = login;         // pobranie i przypisanie loginu nowego użytkownika
-        this.Password = password;   // przypisanie hasła
-        
-    }
-
-    // propercje (pola) czystrzy sposób odczytania i ew zmian w zmiennych klasy
-    public string Login { get; private set; }    // ustawienie Login do odczytu get; i prywatne set; ustawienie
-    public string Password { get; private set; } // jak wyżej dla Password
-
-    public int ResultPoint
-    {
-        get
-        {
-            // getter zwraca podsumowanie punktów listy "score"
-            return this.score.Sum();
-        }
-    }
-
-    // pierwsza metoda - przykładowo dodawanie punktów dla użytkownika
-    public void AddScore(int scorePoint)
-    {
-        // metoda przy każdym wywołaniu dodaje kolejne punkty do listy
-        this.score.Add(scorePoint);
-    }
-}
