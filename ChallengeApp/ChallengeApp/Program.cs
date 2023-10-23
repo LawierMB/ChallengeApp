bf45f52198dcbf860a0c18233b796feaf1f66645
Lawier <lawier@wp.pl>
2023-10-23 13:25:53 +0200

    rozbudowa klasy o listę na punkty i suma zebranych punktów.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,37 +1,50 @@
 ﻿// Pierwsza klasa
-using System.Runtime.CompilerServices;
-using System.Security.AccessControl;
 
 User user1 = new User("Marek", "Password1");
 User user2 = new User("Norbert", "Password2");
 User user3 = new User("Gosia", "Password3");
 User user4 = new User("Damian", "Password4");
 
-
 Console.WriteLine("Uąytkownik 1: " + user1.Login + " " + user1.Password);
 Console.WriteLine("Uąytkownik 3: " + user3.Login + " " + user3.Password);
 
+// przypisanie punktów użytkownikowi user1 przy pomocy metody AddScore()
 user1.AddScore(5);
+user1.AddScore(2);
+user1.AddScore(1);
+
+// wyświetla podsumowanie punktów user1.ResuldPoint
+Console.WriteLine("Zebrane punkty przez użytkownika: " + user1.Login + " to : " + user1.ResultPoint);
 
 class User
 {
-    private int score; // prywatna zmienna przechowująca punkty użytkownika
+    // tworzenie nowego kontenera listy "score" do przechowywania punktów
+    private List<int> score = new List<int>(); 
 
     public User(string login, string password)
     {
         this.Login = login;         // pobranie i przypisanie loginu nowego użytkownika
         this.Password = password;   // przypisanie hasła
-        this.score = 0;             // ustawienie punktacji dla nowego użytkownika
-
+        
     }
 
     // propercje (pola) czystrzy sposób odczytania i ew zmian w zmiennych klasy
     public string Login { get; private set; }    // ustawienie Login do odczytu get; i prywatne set; ustawienie
     public string Password { get; private set; } // jak wyżej dla Password
 
-    // pierwsza metoda - przykładowo dodawanie punktów dla użytkownika
-    public void AddScore(int scorepoint)
+    public int ResultPoint
     {
-        this.score += scorepoint;
+        get
+        {
+            // getter zwraca podsumowanie punktów listy "score"
+            return this.score.Sum();
+        }
+    }
+
+    // pierwsza metoda - przykładowo dodawanie punktów dla użytkownika
+    public void AddScore(int scorePoint)
+    {
+        // metoda przy każdym wywołaniu dodaje kolejne punkty do listy
+        this.score.Add(scorePoint);
     }
 }
