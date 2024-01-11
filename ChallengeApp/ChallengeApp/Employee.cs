e9e51ef83ee357a353ef50002832c096a6add168
Lawier <lawier@wp.pl>
2024-01-11 16:43:03 +0100

    Zadanie dzień 10

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,8 +1,7 @@
 ﻿namespace ChallengeApp
 {
     public class Employee
-    {
-                
+    {       
         private List<float> grades = new List<float>();
 
         public Employee(string FirstName, string LastName) 
@@ -10,16 +9,50 @@
             this.FirstName = FirstName;
             this.LastName = LastName;
         }
-        
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
        
         public void AddGrade(float grade)
         {
             // metoda przy każdym wywołaniu dodaje kolejne punkty do listy
-            this.grades.Add(grade);
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine(grade +" Inwalid grade values !!");
+            }
         }
-
+        public void AddGrade(string grade)
+        {
+            // sprawdza czy grade da się sparsować do float jeśli tak to przypisuje do result
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("This string: " + grade + " is not float !");
+            }
+        }
+        public void AddGrade(int grade)
+        {
+            var result = (float)grade;
+            this.AddGrade(result);  
+        }
+        public void AddGrade(double grade)
+        {
+            if (grade >= float.MinValue && grade <= float.MaxValue)
+            {
+                var result = (double)grade;
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("variable range exceeded: " + grade + " is not float !");
+            }
+        }
         public Statistics GetStatistics()
         {
             var statistics = new Statistics(); // tworzymy obiekt statistics
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -2,9 +2,16 @@
 
 var employee = new Employee("Artem", "Dziarski");
 
-employee.AddGrade(8);
+int gradeint = int.MaxValue;
+double dgrade = double.MaxValue;
+
+employee.AddGrade("2");
+employee.AddGrade("Marek");
+employee.AddGrade("4000");
 employee.AddGrade(3);
 employee.AddGrade(5);
+employee.AddGrade(gradeint);
+employee.AddGrade(dgrade);
 
 var statistic = employee.GetStatistics();
 Console.WriteLine($"Average :{statistic.Average:N2}");
