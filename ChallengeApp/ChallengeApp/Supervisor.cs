aba7744b4feda3d8e02190eece18fbbabf548185
Marek Burzacki <lawier@wp.pl>
2024-04-02 07:24:14 +0200

    Zmiany

---
+++ ChallengeApp/ChallengeApp/Supervisor.cs
@@ -0,0 +1,39 @@
+﻿namespace ChallengeApp
+{
+    public class Supervisor : IEmployee
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Supervisor(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+        public void AddGrade(float grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(string grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(int grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(double grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
